aa4d20c82e59d4c64b89508d3698369eca5a6560
Anthony Towns <aj@erisian.com.au>
2015-03-20 13:43:32 +1000

    add a few extra tests

--- tests/test_bbv1.py
+++ tests/test_bbv1.py
@@ -28,17 +28,22 @@ class FakeSession(object):
         exp_verb, exp_path, exp_params, exp_data = self.expecting
         self.expecting = None
 
+        if data is not None:
+            dec_data = json.loads(data)
+        else:
+            dec_data = None
+
         if exp_verb is _Any: exp_verb = verb
         if exp_path is _Any: exp_path = path
         if exp_params is _Any: exp_params = params
-        if exp_data is _Any: exp_data =data
+        if exp_data is _Any: exp_data = dec_data
 
         if exp_params is None and params == {}:
             exp_params = {}
         if exp_params == {} and params is None:
             exp_params = None
 
-        assert exp_verb == verb and exp_path == path and exp_params == params and exp_data == data
+        assert exp_verb == verb and exp_path == path and exp_params == params and exp_data == dec_data
 
         res_obj = dict(verb=verb, path=path, params=params, data=data)
         return FakeResponse(content = res_obj)
@@ -52,6 +57,9 @@ def test_bb():
     s.expect("GET", "http://www.example.org/path/")
     q = b()
 
+    s.expect("GET", "http://www.example.org/path/", params=dict(a=1, b=2))
+    q = b(a=1, b=2)
+
     s.expect("PUT", "http://www.example.org/path/")
     b._ = None
 
@@ -60,4 +68,7 @@ def test_bb():
 
     s.expect("DELETE", "http://www.example.org/path/")
     del b._
+
+    s.expect("POST", "http://www.example.org/path/foo", data={"a": 1})
+    q = b.foo(dict(a=1))
 
